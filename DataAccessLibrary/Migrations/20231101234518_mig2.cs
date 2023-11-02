c44c35eaea7e49341871531697f90a1f71b011dd
Mücahit Mustafa Usta <mmustafausta@yahoo.com>
2023-11-02 03:16:12 +0300

    add: mig2

---
+++ DataAccessLibrary/Migrations/20231101234518_mig2.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccessLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class mig2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Blogs_Categories_CategoryId",
+                table: "Blogs");
+
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Categories",
+                newName: "CategoryID");
+
+            migrationBuilder.RenameColumn(
+                name: "CategoryId",
+                table: "Blogs",
+                newName: "CategoryID");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Blogs_CategoryId",
+                table: "Blogs",
+                newName: "IX_Blogs_CategoryID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Blogs_Categories_CategoryID",
+                table: "Blogs",
+                column: "CategoryID",
+                principalTable: "Categories",
+                principalColumn: "CategoryID",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Blogs_Categories_CategoryID",
+                table: "Blogs");
+
+            migrationBuilder.RenameColumn(
+                name: "CategoryID",
+                table: "Categories",
+                newName: "Id");
+
+            migrationBuilder.RenameColumn(
+                name: "CategoryID",
+                table: "Blogs",
+                newName: "CategoryId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Blogs_CategoryID",
+                table: "Blogs",
+                newName: "IX_Blogs_CategoryId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Blogs_Categories_CategoryId",
+                table: "Blogs",
+                column: "CategoryId",
+                principalTable: "Categories",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
