5d6a0c3f901b49ca2078458870251ea5162f3778
Mücahit Mustafa Usta <mmustafausta@yahoo.com>
2023-11-03 18:03:58 +0300

    Update Category.cs

--- CoreDemo/Controllers/Category.cs
+++ CoreDemo/Controllers/Category.cs
@@ -1,12 +1,16 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CoreDemo.Controllers
 {
     public class Category : Controller
     {
+        readonly CategoryManager cm = new(new EfCategoryRepository());
         public IActionResult Index()
         {
-            return View();
+            var values = cm.GetList();
+            return View(values);
         }
     }
 }
