e6de49af0b6111dc4cdcf5876af9dd2c90cf5261
Mücahit Mustafa Usta <mmustafausta@yahoo.com>
2023-11-03 16:20:00 +0300

    Update IGenericDal.cs

--- DataAccessLibrary/Abstract/IGenericDal.cs
+++ DataAccessLibrary/Abstract/IGenericDal.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace DataAccessLayer.Abstract
 {
-    internal interface IGenericDal<T> where T : class
+    public interface IGenericDal<T> where T : class
     {
         void Insert(T t);
         void Delete(T t);
