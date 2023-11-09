8192760ee3e40bb30b77d6a150348019c1ef31f7
Mücahit Mustafa Usta <mmustafausta@yahoo.com>
2023-11-09 22:59:04 +0300

    Update HomeController.cs

--- CoreDemo/Controllers/HomeController.cs
+++ CoreDemo/Controllers/HomeController.cs
@@ -1,4 +1,9 @@
-﻿using CoreDemo.Models;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CoreDemo.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
