dcbbc756f562431d55d8b847e9eea475ba0ef979
Mücahit Mustafa Usta <mmustafausta@yahoo.com>
2023-11-09 22:59:02 +0300

    Update CommentController.cs

--- CoreDemo/Controllers/CommentController.cs
+++ CoreDemo/Controllers/CommentController.cs
@@ -1,6 +1,30 @@
-﻿namespace CoreDemo.Controllers
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography.Pkcs;
+
+namespace CoreDemo.Controllers
 {
-	public class CommentController
+	readonly CommentManager cm = new CommentManager(new EfCommentRepository());
+	public class CommentController : Controller
 	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+		public PartialViewResult PartialAddComment()
+		{
+			return PartialView();
+		}
+		public PartialViewResult CommentListByBlog(int id)
+		{
+			var values = cm.GetList(id);
+			return PartialView();
+		}
 	}
 }
