d1e9e653dce74c2f7f1c7c5190ba307e282444e1
Mücahit Mustafa Usta <mmustafausta@yahoo.com>
2023-11-11 03:19:40 +0300

    Update CommentManager.cs

--- BusinessLayer/Concrete/CommentManager.cs
+++ BusinessLayer/Concrete/CommentManager.cs
@@ -11,7 +11,7 @@ namespace BusinessLayer.Concrete
 {
 	public class CommentManager : ICommentService
 	{
-		ICommentDal _commentdal;
+		readonly ICommentDal _commentdal;
 
 		public CommentManager(ICommentDal commentdal)
 		{
