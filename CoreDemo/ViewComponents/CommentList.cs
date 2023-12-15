bde9ffe3fb08661c12b2f4193abcca8b61bc2553
Mücahit Mustafa Usta <mmustafausta@yahoo.com>
2023-12-16 00:40:29 +0300

    feat: View Component

---
+++ CoreDemo/ViewComponents/CommentList.cs
@@ -0,0 +1,29 @@
+﻿using CoreDemo.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDemo.ViewComponents
+{
+    public class CommentList : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var commentvalues = new List<UserComment> {
+                new UserComment
+                {
+                    ID = 1,
+                    Username="Mustafa"
+                },
+                new UserComment
+                {
+                    ID=2,
+                    Username="Murat"
+                }, new UserComment
+                {
+                    ID=3,
+                    Username="Mehmet"
+                }
+                };
+            return View(commentvalues);
+        }
+    }
+}
