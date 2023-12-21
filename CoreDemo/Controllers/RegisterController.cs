b15b3a5a1e110c3a90392e86bec456ba5bfc3c19
Mücahit Mustafa Usta <mmustafausta@yahoo.com>
2023-12-21 03:12:23 +0300

    feat: Httpget and Httppost Attributes and Register Process

---
+++ CoreDemo/Controllers/RegisterController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDemo.Controllers
+{
+	public class RegisterController : Controller
+	{
+		[HttpGet]
+		public IActionResult Index()
+		{
+			
+			return View();
+			
+		}
+		[HttpPost]
+		public IActionResult Index()
+		{
+
+		}
+	}
+}
