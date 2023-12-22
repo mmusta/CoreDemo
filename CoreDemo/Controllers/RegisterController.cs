d70aa7de2d5023d5420adb60112dbbe9cf782c0f
Mücahit Mustafa Usta <mmustafausta@yahoo.com>
2023-12-22 16:17:35 +0300

    feat: writer registration and using sweet alert

--- CoreDemo/Controllers/RegisterController.cs
+++ CoreDemo/Controllers/RegisterController.cs
@@ -1,9 +1,15 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CoreDemo.Controllers
 {
 	public class RegisterController : Controller
 	{
+
+		WriterManager wm = new WriterManager(new EfWriterRepository());
+
 		[HttpGet]
 		public IActionResult Index()
 		{
@@ -12,9 +18,12 @@ namespace CoreDemo.Controllers
 			
 		}
 		[HttpPost]
-		public IActionResult Index()
+		public IActionResult Index(Writer p)
 		{
-
+			p.Status = true;
+			p.About = "Deneme Test";
+			wm.WriteAdd(p);
+			return RedirectToAction("Index","Blog");
 		}
 	}
 }
