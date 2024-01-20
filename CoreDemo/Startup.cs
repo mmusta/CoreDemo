533c9264b5ed9076d49a10ef81716a647b82d4fc
Mücahit Mustafa Usta <mmustafausta@yahoo.com>
2024-01-20 03:33:44 +0300

    feat: authorize and startup.cs configure service

--- CoreDemo/Startup.cs
+++ CoreDemo/Startup.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -17,11 +19,28 @@ namespace CoreDemo
 		{
 			Configuration = configuration;
 		}
+
 		public IConfiguration Configuration { get; }
+
+		// This method gets called by the runtime. Use this method to add services to the container.
 		public void ConfigureServices(IServiceCollection services)
 		{
 			services.AddControllersWithViews();
+
+			services.AddSession();
+
+			services.AddMvc(config =>
+			{
+				var policy = new AuthorizationPolicyBuilder()
+						   .RequireAuthenticatedUser()
+						   .Build();
+				config.Filters.Add(new AuthorizeFilter(policy));
+
+			});
+
 		}
+
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
 		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
 		{
 			if (env.IsDevelopment())
@@ -31,11 +50,27 @@ namespace CoreDemo
 			else
 			{
 				app.UseExceptionHandler("/Home/Error");
+				// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
 				app.UseHsts();
 			}
-			app.UseStatusCodePagesWithReExecute("/ErrorPage/Error1","?code={0}");
+
+			app.UseStatusCodePagesWithReExecute("/ErrorPage/Error1", "?code{0}");
+
+			app.UseHttpsRedirection();
+			app.UseStaticFiles();
+
+			app.UseSession();
+
+			app.UseRouting();
+
+			app.UseAuthorization();
+
+			app.UseEndpoints(endpoints =>
+			{
+				endpoints.MapControllerRoute(
+					name: "default",
+					pattern: "{controller=Home}/{action=Index}/{id?}");
+			});
 		}
-		
-			
-		}
+	}
 }
